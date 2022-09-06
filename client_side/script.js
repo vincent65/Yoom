76e299e5cd617fe3aa564fb3d13ba61b07fc2e44
Vincent Yip <vincentyip@Vincents-MBP.fios-router.home>
2022-09-06 18:45:30 -0400

    client-server communication

--- client_side/script.js
+++ client_side/script.js
@@ -1,21 +1,57 @@
 //client side video streaming code
 
-let myVideoStream;
+const { PeerServer } = require("peer");
+
+
+const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 
-
-//get the user video stream data
-navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-})
-.then((stream) => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream);
+var peer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: "3000",
 });
 
+
+//get the user video stream data
+let myVideoStream;
+navigator.mediaDevices
+  .getUserMedia({
+    audio: true,
+    video: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+    //connect user video stream when they first connect
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+  });
+
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
+};
+
+//send invite to join room
+peer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id, user);
+});
+  
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
     video.addEventListener("loadedmetadata", () => {
--- server.js
+++ server.js
@@ -7,9 +7,22 @@ const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
 //use the view folder for pages
 app.set("view engine", "ejs");
-//access the css
-app.use(express.static("client_side"));
 
+
+//Video Chat and Audio communication and streaming
+//socket.io import; allows to do real time communication between web clients and servers
+const io = require("socket.io")(server);
+//PeerJS allows us to implement  WebRTC: real time communication via API
+const {ExpressPeerServer} = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+app.use("/peerjs", peerServer);
+
+
+//ExpressJS Backend pages
+//access the css 
+app.use(express.static("client_side"));
 //redirect user to distinct room 
 app.get("/", (req, res) => {
     res.redirect(`/${uuidv4()}`);
@@ -19,5 +32,14 @@ app.get("/:room", (req, res) => {
     res.render("room", { roomId: req.params.room });
 });
   
+
+//listen for a join-room event and send user to the room
+io.on("connection", (socket)=>{
+    socket.on("join-room", (roomId, userId)=>{
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", user-Id);
+    });
+});
+
 server.listen(3000);
 
