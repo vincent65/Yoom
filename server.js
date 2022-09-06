7e80da92da41170f11e38fbff593ae6733333a5f
Vincent Yip <vincentyip@Vincents-MBP.fios-router.home>
2022-09-06 17:54:35 -0400

    overall structure: express backend with ejs files as pages

---
+++ server.js
@@ -0,0 +1,12 @@
+//backend server usingn lightweight framework like express JS
+
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World");
+});
+server.listen(3000);
+
+
+app.set('view engine', 'ejs')
