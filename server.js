1fe8a6b4118a201bc861f2cbb7948f4f64c3dfc7
Vincent Yip <vincentyip@Vincents-MBP.fios-router.home>
2022-09-06 18:09:07 -0400

    most of backend logic is done

--- server.js
+++ server.js
@@ -3,10 +3,21 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+//uuid so that each room has unique id 
+const { v4: uuidv4 } = require("uuid");
+//use the view folder for pages
+app.set("view engine", "ejs");
+//access the css
+app.use(express.static("public"));
+
+//redirect user to distinct room 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello World");
+    res.redirect(`/${uuidv4()}`);
+  });
+
+app.get("/:room", (req, res) => {
+    res.render("room", { roomId: req.params.room });
 });
+  
 server.listen(3000);
 
-
-app.set('view engine', 'ejs')
